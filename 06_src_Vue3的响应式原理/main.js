572dd694d48cacea97a3499d8fd01d70d6edc781
Kang Huang <kangder@KangdeMacBook-Pro.local>
2023-03-09 11:10:32 +0800

    Vue中的响应式原理

---
+++ 06_src_Vue3的响应式原理/main.js
@@ -0,0 +1,19 @@
+// 引入的不再是Vue构造函数，引入的是一个名为createApp的工厂函数
+// 区别：构造函数：首字母大写 + new关键字调用  工厂函数直接调用
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 创建应用实例对象 -- app(类似于之前Vue2中的vm，但app比vm更”轻“)
+// app的本质就是一般对象
+// createApp(App).mount('#app')
+const app = createApp(App)
+// 挂载
+app.mount('#app')
+
+// Vue2的写法
+/* const vm = new Vue({
+    render: h => h(App)
+})
+vm.$mount('#app') */
+
+
