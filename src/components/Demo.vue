a36e42900d9cf257a646b1d6460337df4f74d8cd
Kang Huang <kangder@KangdeMBP.lan>
2023-03-13 09:01:05 +0800

    shallow系列

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,4 +1,6 @@
 <template>
+    <h2>当前的x.y是:{{ x.y }}</h2>
+    <button @click="x.y++">点我加1</button>
     <h2>姓名:{{ name }}</h2>
     <h2>年龄:{{ age }}</h2>
     <h2>薪资:{{ job.j1.salary }}</h2>
@@ -8,11 +10,12 @@
 </template>
   
 <script>
-import { reactive, toRef, toRefs } from 'vue'
+import { reactive, ref, toRefs, shallowReactive, shallowRef } from 'vue'
 export default {
     name: 'Demo',
     // setup是没有this的，我们是不需要使用的
     setup() {
+        // let person = shallowReactive({ //只考虑第一层数据的响应式
         let person = reactive({
             name: '章三',
             age: 18,
@@ -22,34 +25,19 @@ export default {
                 }
             }
         })
-        // const name1 = person.name
-        // console.log('%%%', name1)
 
-        // const name2 = toRef(person, 'name')
-        // console.log('###', name2)
-
-        // toRefs -- 因为是将里面的每个数据都弄出去了，很关键
-        const x = toRefs(person)
-        console.log('***', x)
+        // let x = ref(0)
+        // let x = shallowRef(0)
+        let x = shallowRef({ //非响应式的
+            y: 0
+        })
+        console.log('****', x)
+        // let x = ref({
+        //     y: 0
+        // })
 
         return {
-            // person,
-            // person
-            /* name:person.name,  // 这里其实就是字符串
-            age:person.age, 
-            salary:person.job.j1.salary */
-
-            // 上面就相当于此
-            /* name:'章三',
-            age:18, 
-            salary:20 */
-
-            // 常用
-            // name: toRef(person, 'name'),
-            // age: toRef(person, 'age'),
-            // // 第一个参数传一个参数即可
-            // salary: toRef(person.job.j1, 'salary')
-
+            x,
             ...toRefs(person)
         }
     }
